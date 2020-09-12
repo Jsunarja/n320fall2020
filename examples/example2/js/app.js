ee3738e8b6de57d8d677af1b0be17ffb8cb6cda4
Jerremiah Sunarja <jsunarja@iu.edu>
2020-09-12 05:13:27 -0400

    update example 2

--- examples/example2/js/app.js
+++ examples/example2/js/app.js
@@ -1,20 +1,48 @@
-let cx = 10;
-
-class Circle {
-	constructor(x, y, radius, color) {
-		this.x = x;
-		this.y = y;
-		this.radius = radius;
-		this.color = color;
+//Inheritance
+class Band {
+	constructor(instrument, note, duration) {
+		this.instrument = instrument;
+		this.note = note;
+		this.duration = duration;
+	}
+	play() {
+		console.log(
+			this.instrument +
+				" plays " +
+				this.note +
+				" for " +
+				this.duration +
+				" note."
+		);
 	}
 }
-
-function setup() {
-	createCanvas(500, 500);
+class Guitar extends Band {
+	constructor(instrument, note) {
+		super(instrument, note, 1);
+	}
 }
-
-function draw() {
-	cx++;
-	background(42, 32, 125);
-	circle(10, cx, 100);
+class Piano extends Band {
+	constructor(instrument, note) {
+		super(instrument, note, 0.5);
+	}
 }
+let myGuitar = new Guitar("Guitar", "A");
+let myPiano = new Piano("Piano", "G");
+myPiano.play(); //Piano plays G for 0.5 note.
+
+//Abstraction
+class Vehicle {
+	constructor(make, model) {
+		this.make = make;
+		this.model = model;
+	}
+	vehicleType() {
+		return "Vehicle is a " + this.make + " " + this.model;
+	}
+}
+let newVehicle = new Vehicle("Honda", "Civic");
+let myVehicle = newVehicle.vehicleType();
+console.log(myVehicle); //Vehicle is a Honda Civic
+
+//Encapsulation
+class Engine {}
