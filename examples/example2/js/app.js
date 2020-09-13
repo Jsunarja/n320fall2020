588f0c5976539d0fc07eb660dbef074f909766f7
Jerremiah Sunarja <jsunarja@iu.edu>
2020-09-13 14:29:56 -0400

    update example 2

--- examples/example2/js/app.js
+++ examples/example2/js/app.js
@@ -45,4 +45,52 @@ let myVehicle = newVehicle.vehicleType();
 console.log(myVehicle); //Vehicle is a Honda Civic
 
 //Encapsulation
-class Engine {}
+class Employee {
+	setEmpDet(name, id) {
+		this.name = name;
+		this.id = id;
+	}
+	getEmpName() {
+		return this.name;
+	}
+	getEmpId() {
+		return this.id;
+	}
+}
+let emp1 = new Employee();
+emp1.setEmpDet("Adam", 24358);
+console.log("Employee Name: " + emp1.getEmpName());
+console.log("Employee Id: " + emp1.getEmpId());
+
+//Polymorphism
+class Animal {
+	constructor(name, animal) {
+		this.name = name;
+		this.animal = animal;
+	}
+	makeSound() {
+		console.log("Animal Sounds");
+	}
+}
+class Dog extends Animal {
+	constructor(name) {
+		super(name, "dog");
+	}
+	makeSound() {
+		console.log("My " + this.animal + " " + this.name + " says, 'Woof Woof'");
+	}
+}
+class Cat extends Animal {
+	constructor(name) {
+		super(name, "cat");
+	}
+	makeSound() {
+		console.log("My " + this.animal + " " + this.name + " says, 'Meow'");
+	}
+}
+var animal = [];
+animal[0] = new Dog("Brownie");
+animal[1] = new Cat("Kit Kat");
+animal.forEach(function (animal) {
+	animal.makeSound();
+});
