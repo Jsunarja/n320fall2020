e593af333999651ef426e841fedbcf835339a68a
Jerremiah Sunarja <jsunarja@iu.edu>
2020-10-02 16:39:02 -0400

    make lab 4

--- labs/lab4/js/app.js
+++ labs/lab4/js/app.js
@@ -1,31 +1,38 @@
-class Checkers {
-	constructor(board, pieces, player) {
-		this.board = board;
-		this.pieces = pieces;
-		this.player = player;
-	}
-
-	createBoard() {
-        var
-    }
-}
-
-class Board extends Checkers {
-	constructor(board) {
-		super(board);
+class Board {
+	constructor(row1, row2, row3) {
+		this.row1 = row1;
+		this.row2 = row2;
+		this.row3 = row3;
 	}
 }
 
-class Pieces extends Checkers {
-	constructor(pieces) {
-		super(pieces);
+class RedPlayer extends Board {
+	constructor(red) {
+		super(red);
+		this.red = red;
 	}
 }
 
-class Player extends Checkers {
-	constructor(player) {
-		super(player);
+class BluePlayer extends Board {
+	constructor(blue) {
+		super(blue);
+		this.blue = blue;
 	}
 }
 
-document.addEventListener("DOMContentLoaded", createBoard());
+//create elements for board and players
+let board = new Board(["", "", ""], ["", "", ""], ["", "", ""]);
+let playerOne = new RedPlayer("X");
+let playerTwo = new RedPlayer("O");
+
+//get board and apply event listener to click
+var boardStruc = document.getElementsByClassName("board");
+var playerOneX = document.getElementsByClassName("exes");
+for (var i = 0; i < boardStruc.length; i++) {
+	boardStruc[i].addEventListener("click", boxClicked, { once: true });
+}
+
+function boxClicked() {
+	alert("click ");
+	//place mark for player
+}
