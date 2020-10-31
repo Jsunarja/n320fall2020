563963dfc5e30aa4458327558c3a5d69ede82ed2
Jerremiah Sunarja <jsunarja@iu.edu>
2020-10-30 20:27:43 -0400

    adding

--- labs/project1/js/app.js
+++ labs/project1/js/app.js
@@ -1,6 +1,22 @@
+let app = new Vue({
+    el: "#app",
+    method: {
+        //clickingtuff: function(c){
+            //TweenMax.to(columns[c], 0.5, { scaleX: 1.1, scaleY: 1.1 });
+        //},   
+    },
+});
+
 //Get columns and animate when hover over and out
 let columns = document.getElementsByClassName("col");
+let rows = document.getElementsByClassName("row");
+
+let player;
+let rowLength = rows.length;
+let row;
+let column;
 var c;
+
 function mouseOver(c) {
 	TweenMax.to(columns[c], 0.5, { scaleX: 1.1, scaleY: 1.1 });
 }
@@ -8,20 +24,24 @@ function mouseOut(c) {
 	TweenMax.to(columns[c], 0.5, { scaleX: 1, scaleY: 1 });
 }
 function click(c){
-    let row = document.getElementsByClassName("row");
-    var r;
-    for(i=0; i<row.length; i++){
-        colums[c].row[i].classList.add('playerOne');
+
+    for(i=0; i<rowLength; i++){
+        row = rows[i];
+        column = columns[c];
+    };
+    
+    if(rowLength > 0){
+        rowLength--;
+        console.log(rowLength);
     }
+
+    if(player != 1){
+        player = 1;
+        row.classList.add("playerOne");
+        console.log(player);
+    } else if (player = 1){
+        player += 1;
+        row.classList.add("playerTwo");
+        console.log(player);
+    }  
 }
-
-
-let app = new Vue({
-    el: "#app",
-    method: {
-        addButton: function(){
-
-        },   
-    },
-});
-
